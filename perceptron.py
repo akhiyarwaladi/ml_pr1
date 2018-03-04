832b387cc934979ecd94fc464a1665e5c8333bff
akhiyarwaladi <akiyar18@gmail.com>
2018-03-04 19:31:37 +0700

    first commit

---
+++ perceptron.py
@@ -0,0 +1,78 @@
+# Make a prediction with weights
+def predict(row, weights):
+	#print(len(row))
+	activation = weights[0]
+	for i in range(len(row)-1):
+		activation += weights[i + 1] * row[i]
+	print (activation)
+	return 1.0 if activation > 0.0 else 0.0
+
+# Estimate Perceptron weights using stochastic gradient descent
+def train_weights(train, l_rate, n_epoch):
+	weights = [0.0 for i in range(len(train[0]))]
+	print(len(train[0]))
+	# inisialisasi weight
+	weights[0] = 0 # bias
+	weights[1] = 0.2 # w1
+	weights[2] = 0 # w2
+	weights[3] = 0 # w3
+	for epoch in range(n_epoch):
+		print("================================================================")
+		print("               EPOCH" + str(epoch) + "               ")
+		print("================================================================")
+		sum_error = 0.0
+		for row in train:
+			print("row data:"+str(row))
+			prediction = predict(row, weights)
+			error = row[-1] - prediction
+
+			sum_error += error**2
+			# bobot tidak kita update karena soal tidak ada bobot, namun bias tetap ditampilkan di hasil tapi selalu 0					
+			#weights[0] = weights[0] + l_rate * error
+			for i in range(len(row)-1):
+				weights[i + 1] = weights[i + 1] + l_rate * error * row[i]
+			print(["bias", "w1", "w2", "w3"])
+			print(weights)
+			print(" ")
+		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+		print("================================================================")
+		print(" ")
+		if(sum_error == 0.0):
+			break
+	return weights
+
+
+# data dummy hanya untuk testing
+# Calculate weights
+# dataset = [[2.7810836,2.550537003,0],
+# 	[1.465489372,2.362125076,0],
+# 	[3.396561688,4.400293529,0],
+# 	[1.38807019,1.850220317,0],
+# 	[3.06407232,3.005305973,0],
+# 	[7.627531214,2.759262235,1],
+# 	[5.332441248,2.088626775,1],
+# 	[6.922596716,1.77106367,1],
+# 	[8.675418651,-0.242068655,1],
+# 	[7.673756466,3.508563011,1]]
+
+dataset = [[1,0,1,0],
+		[0,0,0,0],
+		[0.5,0,1,1],
+		[1,1,0,1],
+		[0,1,0.5,1]]
+
+
+l_rate = 0.2
+n_epoch = 100
+weights = train_weights(dataset, l_rate, n_epoch)
+print("Weight akhir " + str(weights))
+
+# mulai prediksi data baru
+data1 = [0.5,1,1] #data andi
+data2 = [1,0,0] #data budi
+
+hasil1 = predict(data1, weights)
+hasil2 = predict(data2, weights)
+
+print("Andi diprediksi " + str(hasil1))
+print("Budi diprediksi " + str(hasil2))
---
+++ regression.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+x1 = np.array([1,5,8])
+x2 = np.array([3,2,4])
+y = np.array([5,4,7])
+n = x1.shape[0]
+
+# print(x1.sum())
+# print(x1.shape[0])
+# print(np.square(x1))
+# print(np.square(x2))
+# print(np.square(y))
+# print(np.multiply(x1,x2))
+# print(np.multiply(x1,y))
+# print(np.multiply(x2,y))
+
+sumX1 = x1.sum()
+sumX2 = x2.sum()
+sumY = y.sum()
+
+sumSquareX1 = np.square(x1).sum()
+sumSquareX2 = np.square(x2).sum()
+sumSquareY = np.square(y).sum()
+sumSquareX1X2 = np.multiply(x1,x2).sum()
+sumSquareX1Y = np.multiply(x1,y).sum()
+sumSquareX2Y = np.multiply(x2,y).sum()
+
+sigSumSquareX1 = sumSquareX1 - ((sumX1 ** 2)/ n)
+sigSumSquareX2 = sumSquareX2 - ((sumX2 ** 2) / n)
+sigSumSquareY = sumSquareY - ((sumY ** 2) / n)
+sigSumSquareX1X2 = sumSquareX1X2 - ((sumX1 * sumX2) / n)
+sigSumSquareX1Y = sumSquareX1Y - ((sumX1 * sumY) / n)
+sigSumSquareX2Y = sumSquareX2Y - ((sumX2 * sumY) / n)
+
+print("sigSumSquareX1 "+str(sigSumSquareX1))
+print("sigSumSquareX2 "+str(sigSumSquareX2))
+print("sigSumSquareY "+str(sigSumSquareY))
+print("sigSumSquareX1X2 "+str(sigSumSquareX1X2))
+print("sigSumSquareX1Y "+str(sigSumSquareX1Y))
+print("sigSumSquareX2Y "+str(sigSumSquareX2Y))
+
+b1 = ((sigSumSquareX2 * sigSumSquareX1Y) - (sigSumSquareX2Y * sigSumSquareX1X2)) / ((sigSumSquareX1 * sigSumSquareX2) - (sigSumSquareX1X2 ** 2))
+b2 = ((sigSumSquareX1 * sigSumSquareX2Y) - (sigSumSquareX1Y * sigSumSquareX1X2)) / ((sigSumSquareX1 * sigSumSquareX2) - (sigSumSquareX1X2 ** 2))
+a = (((sumY) - (b1 * sumX1) - (b2 * sumX2)) / n)
+
+print("a "+str(a))
+print("b1 "+str(b1))
+print("b2 "+str(b2))
+
+learning_rate = 0.1
+
+for i in range(2):
+	print("================================================================")
+	print("               EPOCH" + str(i) + "               ")
+	print("================================================================")
+	sumError = 0.0
+	for j in range(x1.shape[0]):
+		print("data ke= "+ str(j))
+		output = a + (b1 * x1[j]) + (b2 * x2[j])
+		error = y[0] - output
+		sumError = sumError + error**2
+		print(output)
+		#print(error)
+		new_a = a + (00000000000.1 * error)
+		new_b1 = b1 + (00000000000.1 * error * x1[j])
+		new_b2 = b2 + (00000000000.1 * error * x2[j])
+
+		a = new_a
+		b1 = new_b1
+		b2 = new_b2
+
+		print("new weight")
+		print("a= " + str(new_a))
+		print("b1= " + str(new_b1))
+		print("b2= " + str(new_b2))
+		print(" ")
+	print("sum error: "+ str (sumError))
+	print("================================================================")
